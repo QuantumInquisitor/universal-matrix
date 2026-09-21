a834f0bc6030dab01edd62b2ec8c4b050c1610db
Matthew Waters <waterslegacytrust@gmail.com>
2026-09-21 03:23:28 -0700

    Align tests with corrected model status semantics

--- tests/test_phase_89.py
+++ tests/test_phase_89.py
@@ -7,7 +7,7 @@ class TestPhase89Quantum(unittest.TestCase):
 
     def test_vqe_hybrid_execution(self):
         res = self.engine.execute_hybrid_vqe([1.57, 0.0], [[1.0, 2.0], [3.0, 4.0]])
-        self.assertEqual(res["status"], "HYBRID_EXECUTION_COMPLETE")
+        self.assertEqual(res["status"], "CLASSICAL_MODULATION_COMPLETE")
         self.assertIn("quantum_expectation", res)
         self.assertEqual(res["hybrid_tensor_shape"], [2, 2])
 
