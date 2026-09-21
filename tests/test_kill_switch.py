761e7f81d6000f62aecb7cf66d556251a21af40c
Matthew Waters <waterslegacytrust@gmail.com>
2026-09-21 03:26:04 -0700

    Align interlock tests with software-request semantics

--- tests/test_kill_switch.py
+++ tests/test_kill_switch.py
@@ -14,8 +14,9 @@ class TestSafetyInterlockKernel(unittest.TestCase):
     def test_thermal_and_current_emergency_trip(self):
         telemetry = TelemetrySnapshot(coil_temp_c=92.0, current_amps=25.0, chassis_displacement_nm=10.0)
         res = self.kernel.evaluate_safety(telemetry)
-        self.assertEqual(res["interlock_status"], "TRIPPED_EMERGENCY_STOP")
-        self.assertTrue(res["hardware_power_cut"])
+        self.assertEqual(res["interlock_status"], "TRIPPED_SOFTWARE_INTERLOCK")
+        self.assertFalse(res["hardware_power_cut"])
+        self.assertTrue(res["hardware_power_cut_requested"])
         self.assertEqual(res["emergency_gcode"], "M112 ; Emergency Stop")
         self.assertEqual(len(res["violations"]), 2)
 
